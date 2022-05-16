0e3ca296e00b65bc75680770680e834473041814
Marcos Galvão <105315627+gruvat@users.noreply.github.com>
2022-05-16 15:40:40 -0300

    return of the teste modification

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,11 +1,7 @@
-import java.text.NumberFormat;
-import java.util.Locale;
-
-public class PrintFormatacaoNumerosLocale {
+public class PrintFormatacaoNumeros {
         public static void main(String[] args) {
             float taxa = 0.2456f;
             float valor = 7654.321f;
-	    NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("en", "BR"));
-            System.out.printf("Valor: %s\nTaxa: %.2f%%\n", nf.format(valor), taxa);
+            System.out.printf("Valor: R$ %,.2f\nTaxa: %.2f%%\n", valor, taxa);
 	}
 }
