673fa8e196df9b96723a284039bc94e479412d79
Marcos Galvão <mgalvao@ciandt.com>
2022-05-23 14:25:00 -0300

    task 2

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+//package maiusculo;
+
+public class Caractere {
+    public static boolean EhMaiusculo(char c) {
+        String upperCaseVar = String.valueOf(c).toUpperCase();
+
+        return upperCaseVar.equals(String.valueOf(c));
+    }
+}
