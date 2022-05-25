cf7f98a55f7414672fc9c636b48c44af768cfe9c
Marcos Galvão <mgalvao@ciandt.com>
2022-05-25 14:42:15 -0300

    task 1 inheritance

---
+++ 0x05/forma/FormaGeometrica.java
@@ -0,0 +1,9 @@
+//package forma;
+
+public class FormaGeometrica {
+
+    public double area() throws java.lang.UnsupportedOperationException {
+        throw new java.lang.UnsupportedOperationException("Funcao Area nao implementada");
+    };
+
+}
