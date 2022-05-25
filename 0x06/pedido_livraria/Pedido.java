308be095b2f4c136bbf942e496165dcf96d59693
Marcos Galvão <mgalvao@ciandt.com>
2022-05-25 18:02:55 -0300

    return changed to string

--- 0x06/pedido_livraria/Pedido.java
+++ 0x06/pedido_livraria/Pedido.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Pedido {
 
     private double percentualDesconto;
@@ -8,14 +10,14 @@ public class Pedido {
         this.itens = itens;
     }
 
-    public double calcularTotal() {
+    public String calcularTotal() {
         double total = 0;
 
         for(ItemPedido item : itens) {
             total += (item.getQuantidade() * item.getProduto().obterPrecoLiquido());
         }
 
-        return total * (1 - (this.percentualDesconto / 100));
+        return String.format(new Locale("pt", "BR"), "%.2f",total * (1 - (this.percentualDesconto / 100)));
     }
 
     public double getPercentualDesconto() {
