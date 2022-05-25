b5dd997808e2985cf33fc25d204a882feb59ba84
Marcos Galvão <mgalvao@ciandt.com>
2022-05-25 14:52:23 -0300

    file package removed

--- 0x05/forma/Program.java
+++ 0x05/forma/Program.java
@@ -1,5 +1,3 @@
-//package forma;
-
 public class Program {
     public static void main(String[] args) {
         FormaGeometrica formaGeometrica = new FormaGeometrica();
@@ -10,4 +8,4 @@ public class Program {
             System.out.println(e);
         }
     }
-}
+}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
