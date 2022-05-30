eb9d96f9b59bb404ff44a2ea1571a3d9b026591a
Marcos Galvão <mgalvao@ciandt.com>
2022-05-30 11:39:23 -0300

    task 2 Lists

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,67 @@
+package todo;
+
+import java.util.ArrayList;
+
+public class ListaTodo {
+
+    private ArrayList<Tarefa> tarefas;
+
+    public ListaTodo() {
+        this.tarefas = new ArrayList<Tarefa>();
+    }
+
+    public void adicionarTarefa(Tarefa tarefa) throws Exception {
+        for (Tarefa t : this.tarefas) {
+            if (t.getIdentificador() == tarefa.getIdentificador()) {
+                throw new Exception(
+                        String.format("Tarefa com identificador %d ja existente na lista", t.getIdentificador())
+                );
+            }
+        }
+
+        this.tarefas.add(tarefa);
+    }
+
+    public boolean marcarTarefaFeita(int identificador) {
+        for (Tarefa t : this.tarefas) {
+            if (t.getIdentificador() == identificador) {
+                t.setEstahFeita(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean desfazerTarefa(int identificador) {
+        for (Tarefa t : this.tarefas) {
+            if (t.getIdentificador() == identificador) {
+                t.setEstahFeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void fazerTodas() {
+        for (Tarefa t : this.tarefas) {
+            t.setEstahFeita(true);
+        }
+    }
+
+    public void desfazerTodas() {
+        for (Tarefa t : this.tarefas) {
+            t.setEstahFeita(false);
+        }
+    }
+
+    public void listarTarefas() {
+        for (Tarefa t : this.tarefas) {
+            String mark = " ";
+            if (t.isEstahFeita()) {
+                mark = "X";
+            }
+            System.out.printf("[%s] Id: %d - Descricao: %s\n", mark, t.getIdentificador(), t.getDescricao());
+        }
+    }
+
+}
---
+++ List/todo/Tarefa.java
@@ -0,0 +1,36 @@
+package todo;
+
+public class Tarefa {
+    private String descricao;
+    private boolean estahFeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador) throws Exception {
+        this.modificarDescricao(descricao);
+        this.identificador = identificador;
+    }
+
+    public void modificarDescricao(String novaDescricao) throws Exception {
+        if (novaDescricao == null || novaDescricao.equals("")) {
+            throw new Exception("Descricao de tarefa invalida");
+        } else {
+            this.descricao = novaDescricao;
+        }
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+}
