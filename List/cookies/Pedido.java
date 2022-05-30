f485a0c37619f8d7c56847a15b27df9c9e0d6852
Marcos Galvão <mgalvao@ciandt.com>
2022-05-30 10:56:39 -0300

    task 1 Lists

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,40 @@
+package cookies;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Pedido {
+
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        cookies = new ArrayList<PedidoCookie>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedido) {
+        this.cookies.add(pedido);
+    }
+
+    public int obterTotalCaixas() {
+        AtomicInteger totalCaixas = new AtomicInteger();
+        this.cookies.forEach(
+                p -> totalCaixas.addAndGet(p.getQuantidadeCaixas())
+        );
+        return totalCaixas.get();
+    }
+
+    public int removerSabor(String sabor) {
+        AtomicInteger removidos = new AtomicInteger();
+        Iterator<PedidoCookie> i = this.cookies.iterator();
+        while(i.hasNext()) {
+            PedidoCookie p = i.next();
+            if (p.getSabor().equals(sabor)) {
+                removidos.addAndGet(1);
+                i.remove();
+            }
+        }
+        return removidos.get();
+    }
+}
