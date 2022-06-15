a486e793ef98a211bf034ea2efecc78c5152de4d
Marcos Galvão <mgalvao@ciandt.com>
2022-06-15 20:31:12 -0300

    task 1 Java Stream exercises

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,30 @@
+package quantidade_pessoas_por_cargo;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        Comparator<String> c = (String p1, String p2) -> p1.compareTo(p2) * -1;
+        return  pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                () -> new TreeMap<>(c),
+                                Collectors.toCollection(TreeSet::new)
+                        )
+                );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                Collectors.counting()
+                        )
+                );
+    }
+
+}
---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/Pessoa.java
@@ -0,0 +1,64 @@
+package quantidade_pessoas_por_cargo;
+
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa>{
+
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        return this.nome.compareTo(o.getNome());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo && idade == pessoa.idade && Double.compare(pessoa.salario, salario) == 0 && Objects.equals(nome, pessoa.nome) && Objects.equals(cargo, pessoa.cargo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f", this.codigo, this.nome, this.cargo, this.idade, this.salario);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+}
