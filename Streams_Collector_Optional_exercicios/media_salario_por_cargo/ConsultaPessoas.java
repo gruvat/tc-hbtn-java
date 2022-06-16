bb3df8bd299ebf7862a641c2055725dcb170d204
Marcos Galvão <mgalvao@ciandt.com>
2022-06-16 18:03:06 -0300

    task 3 Java Stream exercises

---
+++ Streams_Collector_Optional_exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,54 @@
+package media_salario_por_cargo;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        Comparator<String> c = (String p1, String p2) -> p1.compareTo(p2) * -1;
+        return  pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                () -> new TreeMap<>(c),
+                                Collectors.toCollection(TreeSet::new)
+                        )
+                );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                Collectors.counting()
+                        )
+                );
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        //Map< cargo, Map< idade, quantidade>>
+        return pessoas.stream()
+                .collect(
+                  Collectors.groupingBy(
+                          Pessoa::getCargo,
+                          Collectors.groupingBy(
+                                  Pessoa::getIdade,
+                                  Collectors.counting()
+                          )
+                  )
+                );
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                Collectors.averagingDouble(Pessoa::getSalario)
+                        )
+                );
+    }
+
+}
